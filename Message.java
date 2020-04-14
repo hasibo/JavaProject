8321a9f19a50ea0da5ee90ed32bfafe388a03ef1
hasibo <39988600+hasibo@users.noreply.github.com>
2020-04-14 11:25:00 +0200

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+
+public class Message {
+	private Date dateMessage;
+	private int idMessage;
+	private int idUser;
+	private String message;
+	private String user;
+	private int score;
+	
+	public Message(Date dateM,int idM,int idU,String message,String user,int score) {
+		this.dateMessage=dateM;
+		this.idMessage=idM;
+		this.idUser=idU;
+		this.message=message;
+		this.user=user;
+		this.score=score;
+	}
+	
+	public Date getDate() {
+		return this.dateMessage;
+	}
+	
+	public int getIdMessage() {
+		return this.idMessage;
+	}
+	
+	public int getIdUser() {
+		return this.idUser;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+	
+	public String getUser() {
+		return this.user;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public String toString() {
+		return "Message de "+this.user+" du "+this.dateMessage+" : "+this.idMessage;
+	}
+	
+}
