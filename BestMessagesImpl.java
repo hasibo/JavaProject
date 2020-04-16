8c2ae919794a923622fa4022bcc7e85743b5b686
hasibo <39988600+hasibo@users.noreply.github.com>
2020-04-16 12:54:46 +0200

    Update BestMessagesImpl.java

--- BestMessagesImpl.java
+++ BestMessagesImpl.java
@@ -1,11 +1,14 @@
-import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class BestMessagesImpl implements BestMessages{
+public class BestMessagesImpl extends UnicastRemoteObject implements BestMessages{
+	
+	private static final long serialVersionUID = 2674880711467464646L;
 	
 	private ArrayList<Message> bestMessage;
 	
-	public BestMessagesImpl(ArrayList<Message> bestMsg) {
+	public BestMessagesImpl(ArrayList<Message> bestMsg) throws RemoteException{
 		this.bestMessage=bestMsg;
 	}
 	
